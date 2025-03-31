fc705dfe59e10bcf8d8369985e96e90ac3f0f19c
Hussein Mohamad <hussein.mohamad@wearelayer.com>
2025-03-31 15:38:16 +0300

    add fade in/fade out animation to toast

--- review-products/src/app/components/toast/toast.component.ts
+++ review-products/src/app/components/toast/toast.component.ts
@@ -10,4 +10,16 @@ export class ToastComponent {
   @Input() message = '';
   @Input() type: string = 'info';
   @Input() show = false;
+
+  isFadingOut = false;
+
+  ngOnChanges() {
+    if (!this.show) {
+      // Trigger fade-out only when show changes to false
+      this.isFadingOut = true;
+      setTimeout(() => {
+        this.isFadingOut = false;
+      }, 300); // duration must match your CSS fadeOut animation
+    }
+  }
 }
